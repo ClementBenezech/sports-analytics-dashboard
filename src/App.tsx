4f0f98fcf1590ae11c24e29a5a0a1fac10a54e21
Clement Benezech <clementbenezech@gmail.com>
2021-12-16 13:01:28 +0100

    made the custom hook generic so it can be used for all API calls (using typescript generics)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import './App.css';
-import useFetchUserData from './utils/useFetchUserData'
+import useFetchApiData from './utils/useFetchApiData'
+import { userData } from './utils/types/types';
+import { userActivity } from './utils/types/types';
 
 
 function App() {
 
   //Setting up a custom hook to fetch user data
-  const [currentUserApiData] = useFetchUserData("12" as string)
+  const [currentUserApiData]          = useFetchApiData("12" as string, {} as userData )
+  const [currentUserActivityApiData]  = useFetchApiData("12/activity" as string, {} as userActivity )
 
-  if (currentUserApiData === null) {
+  if (currentUserApiData === null || currentUserActivityApiData === null) {
   return (
     <header className="App-header">
       <div>Fetching data, please wait...</div>
     </header>
   )
-  } else if (currentUserApiData === undefined) {
+  } else if (currentUserApiData === undefined || currentUserActivityApiData === undefined) {
     return (
         <div className="App">
         <header className="App-header">
@@ -26,14 +29,15 @@ function App() {
       <div className="App">
       <header className="App-header">
         <div>{currentUserApiData.data.id}</div>
-        <div>{currentUserApiData.data.userInfos.firstName}</div>
+        <div>{currentUserActivityApiData.data.userId}</div>
+        {/*<div>{currentUserApiData.data.userInfos.firstName}</div>
         <div>{currentUserApiData.data.userInfos.lastName}</div>
         <div>{currentUserApiData.data.userInfos.age}</div>
         <div>{currentUserApiData.data.score}</div>
         <div>{currentUserApiData.data.keyData.proteinCount}</div>
         <div>{currentUserApiData.data.keyData.calorieCount}</div>
         <div>{currentUserApiData.data.keyData.lipidCount}</div>
-        <div>{currentUserApiData.data.keyData.carbohydrateCount}</div>
+        <div>{currentUserApiData.data.keyData.carbohydrateCount}</div>*/}
       </header>
       </div>
   )
--- src/utils/useFetchApiData.ts
+++ src/utils/useFetchApiData.ts
@@ -1,23 +1,21 @@
-import { useState, useEffect } from "react";
-import { userData } from "./types/types";
+import { useState, useEffect } from "react"
 
 /* This custom hook fetches data for the user we get as a parameter*/
+/* The return datatype is dynamically determined based on the datatype parameter we pass when using the hook */
+const useFetchApiData = <Type>(query: string, dataType: Type):[Type | null | undefined]  => {
 
-const useFetchUserData = (userId: string) => {
-
-    //Setting up a hook for our API data.
-    // This hook can have 3 types of values:
+    //Setting up a hook for our API data. This hook can have 3 types of values:
     // 1) UserData, the data structure of a valid API response.
     // 2) null, if no call has been done yet.
     // 3) undefined, if an invalid response was received.
-    const [apiData, setApiData] = useState< userData| null | undefined>(null)
+    const [apiData, setApiData] = useState< typeof dataType| null | undefined>(null)
 
     // After each render...
     useEffect(() => {
         // If we do not have data from the API yet, fetch it!
         if (apiData === null) {    
         // Fetching data
-            fetch('http://localhost:3000/user/'+userId, {mode: 'cors'})
+            fetch('http://localhost:3000/user/'+query, {mode: 'cors'})
             .then (response => 
                 response.json())
             .then (response => {
@@ -33,11 +31,11 @@ const useFetchUserData = (userId: string) => {
                 }
         })}
 
-        }, [apiData, userId])
+        }, [apiData, query])
 
   return [apiData]
 
 }
 
-export default useFetchUserData
+export default useFetchApiData
 
